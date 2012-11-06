23a0597d9669d6414e55b17c1c531d8942af7cca
NicoNZ <niconz@casa.fibertel.com.ar>
2012-11-05 23:44:13 -0300

    Some changes made on the php version

--- astar.php
+++ astar.php
@@ -6,62 +6,85 @@
  * 200 = Transitable
  * 42 = obstaculo
  * 15 = fin del mapa
+ * mide "11x20"
  **/
-$mapa = [200,200,200,200,200,200,200,200,200,200,15   ,
-	  200,200,200,200,200,200,200,200,200,200,15   ,  
-	  200,200,200,200,200,200,200,200,200,200,15   ,
-	  200,200,200,200,200,200,200,200,200,200,15   ,
-	  200,200,200,200,200,200,200,200,200,200,15   ,
-	  200,200,200,200,200,200,200,200,200,200,15   ,
-	  200,200,200,200,200,200,200,200,200,200,15   ,
-	  200,200,200,200,200,200,200,200,200,200,15   ,
-	  200,200,042,042,042,042,042,042,042,042,15   ,
-	  200,200,200,200,200,200,200,200,200,200,15   ,
-	  200,200,200,200,200,200,200,200,200,200,15   ,
-	  200,200,200,200,200,200,200,200,200,200,15   ,
-	  200,200,200,200,200,200,200,200,200,200,15   ,
-	  200,200,200,200,200,042,042,042,042,042,15   ,
-	  200,200,200,200,200,042,200,200,200,200,15   ,
-	  200,200,200,200,200,042,200,042,200,200,15   ,
-	  200,200,200,200,200,042,042,042,200,200,15   ,
-	  200,200,200,200,200,200,200,200,200,200,15   ,
-	  200,200,200,200,200,200,200,200,200,200,15   ,
-	  200,200,200,200,200,200,200,200,200,200,15];
+$mapa = array(
+    200,200,200,200,200,200,200,200,200,200,15, // 0
+	  200,200,200,200,200,200,200,200,200,200,15,  // 1
+	  200,200,200,200,200,200,200,200,200,200,15,  // 2
+	  200,200,200,200,200,200,200,200,200,200,15,  // 3
+	  200,200,200,200,200,200,200,200,200,200,15,  // 4
+	  200,200,200,200,200,200,200,200,200,200,15,  // 5
+	  200,200,200,200,200,200,200,200,200,200,15,  // 6
+	  200,200,200,200,200,200,200,200,200,200,15,  // 7
+	  200,200,042,042,042,042,042,042,042,042,15,  // 8
+	  200,200,200,200,200,200,200,200,200,200,15,  // 9
+	  200,200,200,200,200,200,200,200,200,200,15,  // 10
+	  200,200,200,200,200,200,200,200,200,200,15,  // 11
+	  200,200,200,200,200,200,200,200,200,200,15,  // 12
+	  200,200,200,200,200,042,042,042,042,042,15,  // 13
+	  200,200,200,200,200,042,200,200,200,200,15,  // 14
+	  200,200,200,200,200,042,200,042,200,200,15,  // 15
+	  200,200,200,200,200,042,042,042,200,200,15,  // 16
+	  200,200,200,200,200,200,200,200,200,200,15,  // 17
+	  200,200,200,200,200,200,200,200,200,200,15,  // 18
+	  200,200,200,200,200,200,200,200,200,200,15);  //19
 
-$recorrido = [[4,5],[3,6],[2,7],[1,8], [2,9], [3,10], [4,11], [5,12], [4,13], [4,14], [4,15], [4,16], [5,17],[6,17],[7,17], [8,16], [8,15], [7,14], [6,15]];
+$recorrido = array(
+		array(4,5),
+		array(3,6),
+		array(2,7),
+		array(1,8), 
+		array(2,9),
+		array(3,10),
+		array(4,11),
+		array(5,12),
+		array(4,13),
+		array(4,14),
+		array(4,15),
+		array(4,16),
+		array(5,17),
+		array(6,17),
+		array(7,17),
+		array(8,16),
+		array(8,15),
+		array(7,14),
+		array(6,15),
+	);
 
 
 
 
-function getPixel($cx, $cy) {
-	global $mapa;
-    if($mapa[$cy*11 + $cx] == 200) { return true; } else { return false; }
+$output = aStar($mapa, 4, 5, 6, 15);
+
+if($output != $recorrido) {
+  echo "No devuelve el recorrido correcto." . PHP_EOL;
+} else {
+  echo "Devuelve el camino correcto." . PHP_EOL;
 }
 
 
-function popNode(&$lista, $indice) {
-    $return = $lista[$indice];
-    unset($lista[$indice]);
-    foreach($lista as $item){
-    	$listanew[] = $item;
-    }
-    $lista = $listanew;
-    return $return;	
+
+function getPixel($mapa, $cx, $cy) {
+  if(array_key_exists($cy*11 + $cx, $mapa) && $mapa[$cy*11 + $cx] == 200) { 
+    return true; 
+  } else { 
+    return false; 
+  }
 }
 
-function esTransitable($x, $y) {
-    return getPixel($x, $y);
+function esTransitable($mapa, $x, $y) {
+  return getPixel($mapa, $x, $y);
 }
 
 function estaEn($lista, $x, $y) {
-    $encontrado = 0;
-    for($j=0; $j<count($lista); $j++) {
-
-        if( ($lista[$j]["x"] == $x) && ($lista[$j]["y"] == $y) ) {
-            $encontrado = $j + 1;
-        }
+  $encontrado = 0;
+  for($j=0; $j<count($lista); $j++) {
+    if( ($lista[$j]["x"] == $x) && ($lista[$j]["y"] == $y) ) {
+        $encontrado = $j + 1;
     }
-    return $encontrado;
+  }
+  return $encontrado;
 }
 
 function obtenerG($indice) {
@@ -72,19 +95,22 @@ function obtenerF($nodo) {
     return $nodo["g"] + $nodo["h"];
 }
 
-function  popMinCostNode(&$lista) {
+function popMinCostNode(&$lista) {
     $F = 0;
     $indiceMenorF = 0;
     $minCostoF = 100000;
     
     for($i = 0; $i<count($lista); $i++) {
-		$F = obtenerF($lista[$i]);
-		if($F < $minCostoF) {
-			$minCostoF = $F;
-			$indiceMenorF = $i;
-		}
+		  $F = obtenerF($lista[$i]);
+		  if($F < $minCostoF) {
+			  $minCostoF = $F;
+			  $indiceMenorF = $i;
+		  }
     }
-    return popNode($lista,$indiceMenorF);
+    
+    $sustraccion = array_splice($lista, $indiceMenorF, 1);
+    
+    return $sustraccion[0];
   
 } // end nodoAnalizadoMenorCosto
 
@@ -102,25 +128,25 @@ function obtenerH($x, $y, $fx, $fy) {
  *    Integer $fx coordenada x de llegada
  *    Integer $fy coordenada y de llegada
 **/ 
-function aStar($map, $x,$y,$fx,$fy){
-	$nodoActual = ["x" => $x, "y" => $y, "px" => -1, "py" => -1, "g" => 0, "h" => 0 ];
-	// nodo inicial no tiene parent ni F
-	$listaCerrada = [];
-	$listaAbierta = [];
-	$camino = [];
-	$adyacente = [];
+function aStar($mapa, $x,$y,$fx,$fy){
+	$nodoActual = 
+	$listaCerrada = array();
+  //Cargamos el primer nodo, desde donde parte. no tiene parent ni g ni h.
+	$listaAbierta = array(
+	  array("x" => $x, "y" => $y, "px" => -1, "py" => -1, "g" => 0, "h" => 0)
+	);
+	$camino = array();
+	$adyacente = array(
+	  array("x" =>  0, "y" => -1),
+	  array("x" => -1, "y" =>  0),
+	  array("x" =>  0, "y" =>  1),
+	  array("x" =>  1, "y" =>  0),
+	  array("x" => -1, "y" => -1),
+	  array("x" =>  1, "y" =>  1),
+	  array("x" =>  1, "y" => -1),
+	  array("x" => -1, "y" =>  1)
+  );
 
-	$adyacente[0] = [ "x" =>  0, "y" => -1 ];
-	$adyacente[1] = [ "x" => -1, "y" =>  0 ];
-	$adyacente[2] = [ "x" =>  0, "y" =>  1 ];
-	$adyacente[3] = [ "x" =>  1, "y" =>  0 ];
-
-	$adyacente[4] = [ "x" => -1, "y" => -1 ];
-	$adyacente[5] = [ "x" =>  1, "y" =>  1 ];
-	$adyacente[6] = [ "x" =>  1, "y" => -1 ];
-	$adyacente[7] = [ "x" => -1, "y" =>  1 ];
-
-	$listaAbierta[] = $nodoActual;
 	$caminoEncontrado = false;
 
 	while ((count($listaAbierta) !== 0) && (!$caminoEncontrado)) {
@@ -134,7 +160,7 @@ function aStar($map, $x,$y,$fx,$fy){
 				$ax = $nodoAnalizado["x"] + $adyacente[$i]["x"];
 				$ay = $nodoAnalizado["y"] + $adyacente[$i]["y"];
 
-				if (esTransitable($ax, $ay)) {
+				if (esTransitable($mapa, $ax, $ay)) {
 					$indexOpen = estaEn($listaAbierta,$ax,$ay);
 					$indexClose = estaEn($listaCerrada,$ax,$ay);
 
@@ -142,7 +168,7 @@ function aStar($map, $x,$y,$fx,$fy){
 						// no esta en ninguna lista asi que lo agrego en listaAbierta
 						$G = $nodoAnalizado["g"] + obtenerG($i);
 						$H = obtenerH($ax,$ay,$fx,$fy);
-						$nodoTemp = ["x" => $ax, "y" => $ay, "px" => $nodoAnalizado["x"], "py" => $nodoAnalizado["y"], "g" => $G, "h" => $H ];
+						$nodoTemp = array("x" => $ax, "y" => $ay, "px" => $nodoAnalizado["x"], "py" => $nodoAnalizado["y"], "g" => $G, "h" => $H);
 						
 						$listaAbierta[] =  $nodoTemp;
 					}
@@ -156,7 +182,7 @@ function aStar($map, $x,$y,$fx,$fy){
 		return false;
 	} else {
 		$indiceFinal = count($listaCerrada) - 1;
-		array_push($camino, [$listaCerrada[$indiceFinal]["x"], $listaCerrada[$indiceFinal]["y"]]);
+		array_push($camino, array($listaCerrada[$indiceFinal]["x"], $listaCerrada[$indiceFinal]["y"]));
 		$parentX = $listaCerrada[$indiceFinal]["px"];
 		$parentY = $listaCerrada[$indiceFinal]["py"];
 
@@ -165,7 +191,7 @@ function aStar($map, $x,$y,$fx,$fy){
 				if (($listaCerrada[$i]["x"] == $parentX) && ($listaCerrada[$i]["y"] == $parentY)) {
 					$parentX = $listaCerrada[$i]["px"];
 					$parentY = $listaCerrada[$i]["py"];
-					array_push($camino, [$listaCerrada[$i]["x"],  $listaCerrada[$i]["y"]]);
+					array_push($camino, array($listaCerrada[$i]["x"],  $listaCerrada[$i]["y"]));
 					$indiceFinal = $i;
 					break;
 				}
@@ -175,12 +201,4 @@ function aStar($map, $x,$y,$fx,$fy){
 		}
 		return array_reverse($camino);
 	}
-}
-
-$output = aStar($map, 4, 5, 6, 15);
-
-if($output != $recorrido){
-	  echo "No deasvuelve el recorrido correcto";
-}else{
-	  echo "Correcto";
 }
